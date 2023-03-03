6e7a7db51354fdc6ad4226892700b8b725b678ea
Rachael Mugendi <123163541+rachael-mugendi@users.noreply.github.com>
2023-03-03 21:09:09 +0000

    added the pincontroller

---
+++ app/controllers/PinController.php
@@ -0,0 +1,20 @@
+<?php
+
+class PinController extends Model {
+
+  public function __contruct ()
+  {
+    $this->conn = db.connnect()
+  }
+  
+  public function renderView
+  {
+    
+  }
+  
+  public function loadPath()
+  {
+  
+  }
+
+}
