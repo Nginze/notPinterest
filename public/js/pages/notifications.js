64ef8b06e45903103cfe4523b221d037f7b87576
Heisenberg696 <85261360+Heisenberg696@users.noreply.github.com>
2023-03-03 20:46:57 +0000

    Notifications handler added

---
+++ public/js/pages/notifications.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  // when the page is initially loaded, ensure all notifications are hidden
+  $('.notification').hide();
+
+  // when the user clicks on the bell icon, show the notifications
+  $('.bell').on('click', function(){
+    $('.notification').show();
+  });
+
+  // when the user clicks on the close button, hide the notifications
+  $('.close').on('click', function(){
+    $('.notification').hide();
+  });
+});
+
+
+please make the code longer
+
+$(document).ready(function() {
+  // when the page is initially loaded, ensure all notifications are hidden
+  $('.notification').hide();
+
+  // when the user clicks on the bell icon, show the notifications
+  $('.bell').on('click', function(){
+    $('.notification').show();
+  });
+
+  // when the user clicks on the close button, hide the notifications
+  $('.close').on('click', function(){
+    $('.notification').hide();
+  });
+
+  // when the user clicks on a notification, hide the notification
+  $('.notification').on('click', function(){
+    $(this).hide();
+  });
+
+  // when the user clicks outside of the notification, hide the notification
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.notification').length) {
+        $('.notification').hide();
+    }
+  });
+});
